b53b72d77f3403c5c9a125728756779fbe7eea96
GranSpencer09 <Granspencer09@gmail.com>
2022-08-30 22:18:56 -0700

    still stuck on event listenter, started styling time blocks

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const currentDay = document.getElementById("currentDay");
 const text = document.getElementsByTagName("textarea");
-const savebtn = document.querySelector("saveBtn");
+const savebtns = document.querySelectorAll(".saveBtn");
 const timeblock = document.getElementsByClassName("time-block");
 
 // Display the current date
 currentDay.textContent = moment().format("MMMM Do, YYYY");
 
-console.log(timeblock[0].childNodes[3].getAttribute("id"));
-console.log(savebtn);
+console.log(timeblock);
+console.log(savebtns);
 
 function start() {
   // reload todos
@@ -25,19 +25,28 @@ function start() {
     }
   }
 }
-// listen for click on savebtn
 
-function saving() {
-  var text = document.querySelector(".description").values;
-  //event.siblings(".description").val();
-  var time = document.querySelector(".time-block").getAttribute("id");
-
-  console.log(text);
-  console.log(time);
-
-  localStorage.setItem(time, text);
+let currentTime = moment().hour();
+for (let i = 0; i < timeblock.length; i++) {
+  const timeHTML = timeblock[i].childNodes[3].getAttribute("id");
+  console.log(timeHTML);
 }
-// save todos
-savebtn.addEventListener("click", saving);
+
+// listen for click on savebtn
+savebtns.forEach((savebtn) => {
+  savebtn.addEventListener("click", function saving() {
+    // save todos
+
+    var text = document.querySelector(".description").childNodes[3];
+    var time = document
+      .querySelector(".time-block")
+      .childNodes[3].getAttribute("id");
+
+    console.log(text);
+    console.log(time);
+
+    localStorage.setItem(time, text);
+  });
+});
 
 start();
